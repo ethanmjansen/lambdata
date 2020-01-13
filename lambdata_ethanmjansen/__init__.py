0afca399d817918567552f282354da6ec2395463
ethanmjansen <ethanmjansen@users.noreply.github.com>
2020-01-13 16:12:48 -0700

    Added some proof of concept utility files

---
+++ lambdata_ethanmjansen/__init__.py
@@ -0,0 +1,11 @@
+'''
+lambdata - a collection of data science helper functions
+'''
+
+import numpy as np
+import pandas as pd
+
+#sample code
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(50))
+
