da819ce6f370937ceee7ddc0bb90574dfeb70dab
ethanmjansen <ethanmjansen@users.noreply.github.com>
2020-01-14 09:41:51 -0700

    Made version 0.1.2

--- lambdata_ethanmjansen/df_utils.py
+++ lambdata_ethanmjansen/df_utils.py
@@ -3,5 +3,10 @@ utility functions for working with dataframes
 '''
 
 import pandas as pd 
-
+import sklearn
+import sklearn.model_selection
+from sklearn.model_selection import train_test_split
+from scipy.stats import ttest_ind
 TEST_DF = pd.DataFrame([1,2,3])
+
+
